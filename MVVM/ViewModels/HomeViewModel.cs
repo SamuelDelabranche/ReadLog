7ad141da116794b4288b2c8cad24df54d908a7e0
Samuel <118735723+SamuelDelabranche@users.noreply.github.com>
2024-09-30 21:55:56 +0200

    GetMangaByName & isMangaExist method

--- MVVM/ViewModels/HomeViewModel.cs
+++ MVVM/ViewModels/HomeViewModel.cs
@@ -12,8 +12,39 @@ namespace ReadLog.MVVM.ViewModels
 {
     public class HomeViewModel : ViewModelBase
     {
-        public HomeViewModel(INavigationService navigationService, DataStore<Manga> dataStore) : base(navigationService, dataStore)
+        private readonly IMangaApiClient _mangaApiClient;
+
+
+        private string _APIStatus;
+        public string APIStatus
         {
+            get
+            {
+                return _APIStatus;
+            }
+            set
+            {
+                _APIStatus = value;
+                OnPropertyChanged(nameof(APIStatus));
+            }
+        }
+        public HomeViewModel(INavigationService navigationService, DataStore<Manga> dataStore, IMangaApiClient mangaApiClient) : base(navigationService, dataStore)
+        {
+            _mangaApiClient = mangaApiClient;
+            InitializeAsync();
+        }
+
+        private async void InitializeAsync()
+        {
+            bool status = await getAPIStatus();
+            if (status) { APIStatus = "Ok"; } else { APIStatus = "Not Ok"; }
+        }
+
+        private async Task<bool> getAPIStatus()
+        {
+            var status = await _mangaApiClient.isMangaExist("Chainsaw Man");
+            return status;
+
         }
     }
 }
